e0cacd0a7b947b4f12b8d491ec9c4826d6dbadd1
Danny Milosavljevic <dannym@friendly-machines.com>
2024-08-04 23:28:16 +0200

    Clean up.

--- crates/driver/src/main.rs
+++ crates/driver/src/main.rs
@@ -25,7 +25,7 @@ use rustc_session::Session;
 struct MyCallbacks;
 
 impl MyCallbacks {
-    fn qtypeck<'tcx>(ctxt: TyCtxt<'tcx>, def_id: LocalDefId) -> &'tcx ty::TypeckResults<'tcx> {
+    fn typeck(ctxt: TyCtxt, def_id: LocalDefId) -> &ty::TypeckResults {
         let providers = Providers::default();
         //rustc_driver::default_provide(&mut providers);
         let result = (providers.typeck)(ctxt, def_id).to_owned();
@@ -34,36 +34,14 @@ impl MyCallbacks {
 
         result
     }
-    fn qoverride_queries(session: &Session, providers: &mut Providers) {
-        //let old_check = providers.typeck;
-        //providers.typeck = |ctxt, def_id| old_check(ctxt, def_id);
-        //(providers.analysis)()
-        providers.typeck = MyCallbacks::qtypeck;
-        /* FIXME if let Some(original) = original_queries.as_ref() {
-            original(session, providers);
-        }*/
-
-        // Override specific query provider(s) here.
-        /*        let original_typeck_tables_of = providers.typeck_tables_of;
-        //std::mem::replace(&mut local_providers.typeck_tables_of, my_custom_typeck_tables_of);
-
-        providers.typeck_tables_of = move |ctx, id| {
-            // First, run regular type inference, i.e. the default Providers.typeck_tables_of(ctx, id).
-            //let mut providers = rustc::ty::maps::Providers::default();
-            //rustc_driver::driver::default_provide(&mut providers);
-            let tables = original_typeck_tables_of(ctx, id);
-            //let tables = (providers.typeck_tables_of)(ctx, id);
-            let mut analyzer = dimanalysis::DimAnalyzer::new(ctx, tables, id);
-            analyzer.analyze();
-            tables
-        };*/
+    fn override_queries(session: &Session, providers: &mut Providers) {
+        providers.typeck = Self::typeck;
     }
 }
 
 impl Callbacks for MyCallbacks {
     fn config(&mut self, config: &mut rustc_interface::Config) {
-        let original_queries = config.override_queries.take();
-        config.override_queries = Some(MyCallbacks::qoverride_queries);
+        config.override_queries = Some(Self::override_queries);
     }
 }
 
